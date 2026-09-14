dae885147a664aaf2b6e86ca5a38e8075f49b5ad
Ambata10S-ay2627 <ambata.jeb@obmontessori.edu.ph>
2026-09-14 11:55:32 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# Receipt Generator
+from pyscript import display, document
+
+
+def create_order(e):
+    prod1 = document.getElementById("item1")
+    prod2 = document.getElementById("item2")
+    prod3 = document.getElementById("item3")
+    prod4 = document.getElementById("item4")
+
+    subtotal = float(prod1.value) * prod1.checked + float(prod2.value) * prod2.checked + float(prod3.value) * prod3.checked + float(prod4.value) * prod4.checked
+    display(subtotal, target="show") 
